d193d0d500c560b5fcdd1c6da4c9b47cf0962ba3
wonho <adler0311@gmail.com>
2020-10-19 14:00:09 +0900

    refactor: post view 인증 처리 리팩토링

--- backend/views/posts_view.py
+++ backend/views/posts_view.py
@@ -1,6 +1,7 @@
 from flask import request, jsonify
 from flask_classful import FlaskView
 from backend.models.post import Post
+from backend.models.user import User
 from backend.models.auth_token import AuthToken
 from backend.schemas.post_schema import PostSchema
 from mongoengine import DoesNotExist
@@ -9,6 +10,24 @@ import logging
 
 posts_schema = PostSchema(many=True)
 post_schema = PostSchema()
+
+
+def token_required(func):
+    def wrapper(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if token is None:
+            return jsonify({'message': 'token required'}), 401
+
+        try:
+            auth_token = AuthToken.objects.get(token=token)
+
+        except DoesNotExist as e:
+            return jsonify({'message': 'not authenticated'}), 401
+
+        kwargs['auth_token'] = auth_token
+        return func(*args, **kwargs)
+
+    return wrapper
 
 
 class PostsView(FlaskView):
@@ -22,30 +41,6 @@ class PostsView(FlaskView):
             logging.debug(e)
             return 'Internal Server Error', 500
 
-    def post(self):
-        token = request.headers.get('Authorization')
-        if token is None:
-            return jsonify({'message': 'token required'}), 401
-
-        try:
-            auth_token = AuthToken.objects.get(token=token)
-        except DoesNotExist as e:
-            return jsonify({'message': 'not authenticated'}), 401
-
-        json_data = request.get_json()
-        if not json_data:
-            return jsonify({'message': 'No input data provided'}), 400
-
-        try:
-            data = post_schema.load(json_data)
-        except ValueError as err:
-            return jsonify(err.messages), 422
-
-        p = Post(**data)
-        p.save()
-
-        return {'result': True}, 201
-
     def get(self, id):
         try:
             post = Post.objects.get(pk=id)
@@ -54,11 +49,9 @@ class PostsView(FlaskView):
         except DoesNotExist as e:
             return jsonify({'message': 'Post matching query does not exist'}), 404
 
-    def put(self, id):
-        token = request.headers.get('Authorization')
-        if token is None:
-            return jsonify({'message': 'token required'}), 401
-
+    @token_required
+    def post(self, **kwargs):
+        auth_token = kwargs['auth_token']
         json_data = request.get_json()
         if not json_data:
             return jsonify({'message': 'No input data provided'}), 400
@@ -68,10 +61,24 @@ class PostsView(FlaskView):
         except ValueError as err:
             return jsonify(err.messages), 422
 
+        data['writer'] = auth_token.user
+        p = Post(**data)
+        p.save()
+
+        return {'result': True}, 201
+
+    @token_required
+    def put(self, id, **kwargs):
+        auth_token = kwargs['auth_token']
+
+        json_data = request.get_json()
+        if not json_data:
+            return jsonify({'message': 'No input data provided'}), 400
+
         try:
-            auth_token = AuthToken.objects.get(token=token)
-        except DoesNotExist as e:
-            return jsonify({'message': 'not authenticated'}), 401
+            data = post_schema.load(json_data)
+        except ValueError as err:
+            return jsonify(err.messages), 422
 
         post = Post.objects.get(pk=id)
 
@@ -86,15 +93,9 @@ class PostsView(FlaskView):
 
         return {'result': True}, 200
 
-    def delete(self, id):
-        token = request.headers.get('Authorization')
-        if token is None:
-            return jsonify({'message': 'token required'}), 401
-
-        try:
-            auth_token = AuthToken.objects.get(token=token)
-        except DoesNotExist as e:
-            return jsonify({'message': 'not authenticated'}), 401
+    @token_required
+    def delete(self, id, **kwargs):
+        auth_token = kwargs['auth_token']
 
         post = Post.objects.get(pk=id)
         if post.writer.id != auth_token.user.id:
